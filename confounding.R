33150cdf6fcee790c3c115e719afdc78a78d3ec2
Thomas Ahern <47900335+tpahern@users.noreply.github.com>
2020-03-27 13:07:54 -0400

    upper limit on rr.eu changed from 2 to 1.08

--- confounding.R
+++ confounding.R
@@ -4,7 +4,7 @@ rm(list=ls())
 cat("\014") # clears console
 
 p1=.0774 #prev of family history in US population
-rr.eu <- seq(0.5, 2, length=40) #vary association between hormone contraception use and famhx
+rr.eu <- seq(0.5, 1.08, length=40) #vary association between hormone contraception use and famhx
 p0=p1/rr.eu #estimate family history prevalence in nonusers of hormone contraception
 rr.ud <- 2 #association between family history and breast cancer
 rr.obs=1.2 #crude hormone contraception/breast cancer association
